81c7f10d3442794305723aa82349e95331a1ad8f
kameshX007 <kameshbook@gmail.com>
2022-09-25 21:39:21 +0530

    #

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # deployCoadingWS
 This script will deploy a VS code Kasm docker container with firefox on ubuntu desktop
-  - It will use port 49152
+  - It will use port 6901
   - User name - kasm_user
   - VNC password as per script input
--- deployCoadingWS.sh
+++ deployCoadingWS.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 #Checking whether VNC password is passed to script
 vncPassword=$2;
-[ -z "$vncPassword" ] && echo "###Please pass VNC password...!!!">deployCoadingWS.log && exit 0 || echo "Successfully got VNC password...">deployCoadingWS.log;
+[ -z "$vncPassword" ] && echo "Please pass VNC password...!!!" && exit 0 || echo "Successfully got VNC password...";
 
 #Checking whether username is passed to script
 #A docker folder will be created inside this users home directory and container persistant data will be stored there
 dockerUser=$1;
-[ -z "$dockerUser" ] && echo "###Please pass username...!!!">>deployCoadingWS.log && exit 0 || echo "Deploying container for user $dockerUser">>deployCoadingWS.log;
+[ -z "$dockerUser" ] && echo "###Please pass username...!!!" && exit 0 || echo "Deploying container for user $dockerUser";
 
 #Checking & Removing existing container
-echo "Checking if coadingws already deployed...">>deployCoadingWS.log;
-container=$(docker ps -q --filter ancestor=coadingws )>>deployCoadingWS.log;
-[ -z "$container" ] && echo "No running container found">>deployCoadingWS.log || docker rm -f $container;
+echo "Checking if coadingws already deployed...";
+container=$(docker ps -q --filter ancestor=coadingws );
+echo $container;
+[ -z "$container" ] && echo "No running container found" || docker rm -f $container;
 
 #Building image
-echo "Building coadingws image...">>deployCoadingWS.log;
+echo "Building coadingws image...";
 docker build . --file Dockerfile --tag coadingws;
-echo "Image build successfully...">>deployCoadingWS.log;
+echo "Image build successfully...";
 
 #Deploying container
-echo "Deploying VScode...">>deployCoadingWS.log;
-docker run -d -p 49152:6901 -e VNC_PW=$vncPassword --restart=always -v /home/$dockerUser/docker/CoadingWS/kasm-user:/home/kasm-user --name coadingwscontainer coadingws;
-echo "coadingws deployment successfull...">>deployCoadingWS.log;
+echo "Deploying VScode...";
+docker run -d -p 6901:6901 -e VNC_PW=$vncPassword --restart=always -v /home/$dockerUser/docker/CoadingWS/kasm-user:/home/kasm-user --name coadingwscontainer coadingws;
+echo "coadingws deployment successfull...";
 exit 0
